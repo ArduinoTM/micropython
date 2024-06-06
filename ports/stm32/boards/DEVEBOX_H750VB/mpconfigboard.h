3fcc4dca5daa0aee807e5132fe1151c75e618c9e
ArduinoTM <1579115172@qq.com>
2024-06-06 17:12:35 +0800

    new file:   ports/stm32/boards/DEVEBOX_H750VB/bdev.c                Add QSPI Flash Support 	modified:   ports/stm32/boards/DEVEBOX_H750VB/board_init.c          Add QSPI Flash Support 	modified:   ports/stm32/boards/DEVEBOX_H750VB/mpconfigboard.h       Add QSPI Flash Support 	modified:   ports/stm32/boards/DEVEBOX_H750VB/pins.csv              Add QSPI Flash Support

--- ports/stm32/boards/DEVEBOX_H750VB/mpconfigboard.h
+++ ports/stm32/boards/DEVEBOX_H750VB/mpconfigboard.h
@@ -11,16 +11,23 @@
 #define MICROPY_HW_MCU_NAME         "STM32H750VB"
 #define MICROPY_HW_FLASH_FS_LABEL   "DEVEBOXH7"
 
+// 1 = use internal flash (1 MByte)
+// 0 = use onboard SPI flash (512 KByte) Winbond W25X40
+#define MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE (0)
+
 #define MICROPY_HW_ENABLE_RTC       (1)
 #define MICROPY_HW_ENABLE_RNG       (1)
+#define MICROPY_HW_ENABLE_TIMER     (1)
 #define MICROPY_HW_ENABLE_ADC       (1)
 #define MICROPY_HW_ENABLE_DAC       (1)
 #define MICROPY_HW_ENABLE_USB       (1)
 #define MICROPY_HW_ENABLE_SDCARD    (1)
 #define MICROPY_HW_HAS_SWITCH       (1)
 #define MICROPY_HW_HAS_FLASH        (1)
+#define MICROPY_FATFS_EXFAT         (1)
 
-#define MICROPY_BOARD_EARLY_INIT    VCC_GND_STM32H743VI_board_early_init
+#define MICROPY_BOARD_EARLY_INIT    DEVEBOX_H750VB_board_early_init
+void DEVEBOX_H750VB_board_early_init(void);
 
 // The board has an 25MHz HSE, the following gives 480MHz CPU speed
 #define MICROPY_HW_CLK_PLLM         (5)
@@ -78,16 +85,43 @@
 #define MICROPY_HW_SPI2_MISO        (pin_B14)
 #define MICROPY_HW_SPI2_MOSI        (pin_B15)
 
-// USRSW is pulled low. Pressing the button makes the input go high.
-#define MICROPY_HW_USRSW_PIN        (pin_B3)
-#define MICROPY_HW_USRSW_PULL       (GPIO_NOPULL)
-#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_RISING)
-#define MICROPY_HW_USRSW_PRESSED    (1)
+// USRSW is pulled high. Pressing the button makes the input go low.
+#define MICROPY_HW_USRSW_PIN        (pin_E3)
+#define MICROPY_HW_USRSW_PULL       (GPIO_PULLUP)
+#define MICROPY_HW_USRSW_EXTI_MODE  (GPIO_MODE_IT_FALLING)
+#define MICROPY_HW_USRSW_PRESSED    (0)
 
 // LEDs
-#define MICROPY_HW_LED1             (pin_A13)   // red
+#define MICROPY_HW_LED1             (pin_A1)   // yello
 #define MICROPY_HW_LED_ON(pin)      (mp_hal_pin_low(pin))
 #define MICROPY_HW_LED_OFF(pin)     (mp_hal_pin_high(pin))
+
+// If using onboard SPI flash
+#if !MICROPY_HW_ENABLE_INTERNAL_FLASH_STORAGE
+
+// W25X40 SPI Flash = 64 Mbit (8 MByte)
+// #define MICROPY_HW_SPIFLASH_SIZE_BITS (64 * 1024 * 1024)
+// #define MICROPY_HW_SPIFLASH_CS      (pin_B6)
+// #define MICROPY_HW_SPIFLASH_SCK     (pin_B2)
+// #define MICROPY_HW_SPIFLASH_MISO    (pin_D11)
+// #define MICROPY_HW_SPIFLASH_MOSI    (pin_D12)
+#define MICROPY_HW_SPIFLASH_SIZE_BITS   (64 * 1024 * 1024)
+#define MICROPY_HW_SPIFLASH_CS         (pin_B6)
+#define MICROPY_HW_SPIFLASH_SCK        (pin_B2)
+#define MICROPY_HW_SPIFLASH_IO0        (pin_D11)
+#define MICROPY_HW_SPIFLASH_IO1        (pin_D12)
+#define MICROPY_HW_SPIFLASH_IO2        (pin_E2)
+#define MICROPY_HW_SPIFLASH_IO3        (pin_D13)
+
+extern const struct _mp_spiflash_config_t spiflash_config;
+extern struct _spi_bdev_t spi_bdev;
+#define MICROPY_HW_SPIFLASH_ENABLE_CACHE (1)
+#define MICROPY_HW_BDEV_SPIFLASH    (&spi_bdev)
+#define MICROPY_HW_BDEV_SPIFLASH_CONFIG (&spiflash_config)
+#define MICROPY_HW_BDEV_SPIFLASH_SIZE_BYTES (MICROPY_HW_SPIFLASH_SIZE_BITS / 8)
+#define MICROPY_HW_BDEV_SPIFLASH_EXTENDED (&spi_bdev) // for extended block protocol
+
+#endif
 
 // USB config
 #define MICROPY_HW_USB_FS           (1)
@@ -103,5 +137,3 @@
 //#define MICROPY_HW_SDCARD_DETECT_PIN        (pin_A8)
 //#define MICROPY_HW_SDCARD_DETECT_PULL       (GPIO_PULLUP)
 //#define MICROPY_HW_SDCARD_DETECT_PRESENT    (GPIO_PIN_RESET)
-
-void VCC_GND_STM32H743VI_board_early_init(void);
